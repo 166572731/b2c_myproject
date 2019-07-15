e6f0a4f6f8585e0a72417b4a45549107881c7489
166572731 <166572731@qq.com>
2019-07-15 17:20:30 +0800

    陈磊--后台模板更新

--- b2c_merchant/app.js
+++ b2c_merchant/app.js
@@ -4,8 +4,27 @@ var app  = express()
 //静态资源直接访问
 app.use(express.static('public'))
 app.use(express.static('views'))
-var apiProxy = proxy('/demodate', { target: 'http://localhost:8888',changeOrigin: true });//将服务器代理到localhost:8888端口上
-app.use('/demodate/*', apiProxy);
+
+/*陈磊*/
+var apiProxy1 = proxy('/chen', { target: 'http://b2c.free.idcfengye.com',changeOrigin: true });//将服务器代理到localhost:8888端口上
+app.use('/chen/*', apiProxy1);
+
+/*喻俊*/
+var apiProxy2 = proxy('/yujun', { target: 'http://yujunzuul.free.idcfengye.com',changeOrigin: true });
+app.use('/yujun/*', apiProxy2);
+
+/*甘家辉*/
+var apiProxy3 = proxy('/gan', { target: 'http://localhost:7777',changeOrigin: true });
+app.use('/gan/*', apiProxy3);
+
+/*彭文浩*/
+var apiProxy4 = proxy('/peng', { target: 'http://pengzuul.free.idcfengye.com',changeOrigin: true });
+app.use('/peng/*', apiProxy4);
+
+/*肖新雨*/
+var apiProxy5 = proxy('/xiao', { target: 'http://localhost:5555',changeOrigin: true });
+app.use('/xiao/*', apiProxy5);
+
 app.listen(8181,function(){
     console.log('8181 connect successfully')
 })
